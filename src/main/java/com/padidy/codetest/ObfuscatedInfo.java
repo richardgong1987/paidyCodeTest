a386292605a38e28165958d90575d240dddd2d96
gong.hanjin <richardgong1988@gmail.com>
2022-04-21 22:18:09 +0900

    feat: add OdinalSuffix phone 100%

--- src/main/java/com/padidy/codetest/ObfuscatedInfo.java
+++ src/main/java/com/padidy/codetest/ObfuscatedInfo.java
@@ -5,11 +5,20 @@ import java.util.regex.Pattern;
 
 public class ObfuscatedInfo {
 	public static void main(String[] args) {
-		String s = obfuscatedEmail("local-part@domain-name.com");
-		System.out.println(s.equals("l*****t@domain-name.com"));
+		{
+			String s = obfuscatedEmail("local-part@domain-name.com");
+			System.out.println(s.equals("l*****t@domain-name.com"));
+		}
+		{
+			String s = obfuscatedPhone("+44 123 456 789");
+			System.out.println(s.equals("+**-***-**6-789"));
+		}
+
+
 	}
 
 	public static String emailPattern = "^[a-zA-Z0-9_+&*-]+(?:\\." + "[a-zA-Z0-9_+&*-]+)*@" + "(?:[a-zA-Z0-9-]+\\.)+[a-z" + "A-Z]{2,7}$";
+	public static String phonePattern = "^\\+?[0-9 ]{9,}$";
 
 	public static String obfuscatedEmail(String email) {
 		if (isBlank(email)) {
@@ -37,7 +46,28 @@ public class ObfuscatedInfo {
 			return phone;
 		}
 		phone = phone.trim();
-		return "";
+		if (patternMatches(phone, phonePattern)) {
+			phone = phone.replaceAll("\\s+", "-");
+			StringBuilder retPhone = new StringBuilder();
+			int len = phone.length();
+			var targetLen = 0;
+			for (int j = len - 1; j >= 0; j--) {
+				var c = phone.charAt(j) + "";
+				boolean isNumber = Pattern.compile("\\d").matcher(c).matches();
+				if (isNumber) {
+					if (targetLen < 4) {
+						retPhone.insert(0, c);
+						targetLen++;
+					} else {
+						retPhone.insert(0, "*");
+					}
+				} else {
+					retPhone.insert(0, c);
+				}
+			}
+			return retPhone.toString();
+		}
+		return phone;
 	}
 
 	private static boolean isBlank(String str) {
